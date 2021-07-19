9049dca3121dc962808555e4cb52fe6b1534bd1f
Sairash <sairash0323@gmail.com>
2021-07-19 07:47:13 +0545

    sum program added

--- two.c
+++ two.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    int i;
+    int i,sum,a,b;
     for(i=1;i<=10;i++)
     {
         printf("\t %d",i);
     }
+
+    a=5;
+    b=8;
+    sum=a+b;
+    printf("The sum is %d",sum);
 }
